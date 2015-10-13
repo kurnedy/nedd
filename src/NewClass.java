112f9d2217061bbb1c9154f59e13b3727a8aca4c
kurnedy <Knedy0449@gmail.com>
2015-10-13 04:54:32 -0700

    tugas-2

---
+++ src/NewClass.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author deny setiawan
+ */
+public class NewClass {
+    public int hasil ;
+    int penjumlahan (int a, int b){
+        hasil = a+b;
+        return hasil;
+    }
+    int pengurangan (int a, int b){
+        hasil = a-b;
+        return hasil;
+    }
+    int perkalian (int a, int b){
+        hasil = a*b;
+        return hasil;
+    }
+    double pembagian (double a, double b){
+        double hasil = (a/b);
+        return hasil;
+    }
+    
+}
